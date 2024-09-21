afc1fda65e44db806f7b52321bf15b4278bb8b18
Joshtri <stuffofyos1516@gmail.com>
2024-09-21 19:40:04 +0800

    updating all

--- src/components/Publisher/PublisherAbout.jsx
+++ src/components/Publisher/PublisherAbout.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import publisherImg from '../../assets/publisherImg.jpg';
 
 function PublisherAbout() {
--- src/pages/Service/ProgressSection.jsx
+++ src/pages/Service/ProgressSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import Layout from '../Layout'
 import ProgressBukuSection from '../../components/Service/ProgressBukuSection'
 
